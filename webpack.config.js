434e83ca80a13a03146cfd6a49f41fab4230b5d9
Biamah <beatriz.souza.costa@outlook.com>
2021-12-04 21:47:32 -0300

    chore: configura webpack loader-css

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,17 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
 module.exports = {
-    // entry: 'index.js',
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: 'main.js'
     },
     plugins: [new HtmlWebpackPlugin()],
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+          },
+        ],
+      },
 }
